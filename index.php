ab56998afa50db6513a2dc1cb16003235ecf5981
Denis-92 <d.mozenic@gmail.com>
2022-10-12 16:47:31 +0200

    move text to censor into variable

--- index.php
+++ index.php
@@ -10,21 +10,26 @@ Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
 
 $wordToCover= $_GET["censoredWord"];
 
+$textToScan="
+
+Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+Earum maiores quasi maxime tempora, iusto eos, vitae soluta saepe commodi atque excepturi?
+Dignissimos odit ipsum ad placeat nostrum cupiditate qui minus!
+
+Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+Ex esse sed quis ad quo, qui atque voluptates quidem a minima dignissimos iusto placeat illo,
+totam, ut aspernatur odit reprehenderit! Repudiandae?
+
+";
+
 ?>
 
-<p>
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-    Earum maiores quasi maxime tempora, iusto eos, vitae soluta saepe commodi atque excepturi?
-    Dignissimos odit ipsum ad placeat nostrum cupiditate qui minus!
-</p>
-
-<p>
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-    Ex esse sed quis ad quo, qui atque voluptates quidem a minima dignissimos iusto placeat illo,
-    totam, ut aspernatur odit reprehenderit! Repudiandae?
-</p>
 
 <p>
     La parola da censurare è:
     <?php echo $wordToCover ?>
+</p>
+
+<p>
+    <?php echo $textToScan ?>
 </p>

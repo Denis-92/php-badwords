b7cf888a8d4150a43bb65bd12a65051ca52846bc
Denis-92 <d.mozenic@gmail.com>
2022-10-12 17:02:45 +0200

    modify name variables and add explode method

--- index.php
+++ index.php
@@ -1,16 +1,22 @@
-<!-- Esercizio di oggi: PHP Badwords
+<!--
+    Esercizio di oggi: PHP Badwords
 
-Descrizione:
-Creare una variabile con un paragrafo di testo a vostra scelta.
-Stampare a schermo il paragrafo e la sua lunghezza.
-Una parola da censurare viene passata dall'utente tramite parametro GET.
-Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
+    Descrizione:
+
+    Creare una variabile con un paragrafo di testo a vostra scelta.
+
+    Stampare a schermo il paragrafo e la sua lunghezza.
+    Una parola da censurare viene passata dall'utente tramite parametro GET.
+    
+    Stampare di nuovo il paragrafo e la sua lunghezza,
+    dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+-->
 
 <?php
 
-$wordToCover= $_GET["censoredWord"];
+$wordToHide = $_GET["word"];
 
-$textToScan="
+$textToScan = "
     Lorem ipsum, dolor sit amet consectetur adipisicing elit.
     Earum maiores quasi maxime tempora, iusto eos, vitae soluta saepe commodi atque excepturi?
     Dignissimos odit ipsum ad placeat nostrum cupiditate qui minus!
@@ -20,12 +26,14 @@ $textToScan="
     totam, ut aspernatur odit reprehenderit! Repudiandae?
 ";
 
+$cleanTextArray = explode ( $wordToHide , $textToScan );
+
 ?>
 
 
 <p>
     La parola da censurare è:
-    <?php echo $wordToCover ?>
+    <?php echo $wordToHide ?>
 </p>
 
 <p>

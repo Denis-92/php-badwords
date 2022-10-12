c09df49af9e21b3f84c09d997332dce94b07ba73
Denis-92 <d.mozenic@gmail.com>
2022-10-12 16:29:38 +0200

    add lorem text

--- index.php
+++ index.php
@@ -11,3 +11,15 @@ Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
 $_GET["censoredWord"]
 
 ?>
+
+<p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+    Earum maiores quasi maxime tempora, iusto eos, vitae soluta saepe commodi atque excepturi?
+    Dignissimos odit ipsum ad placeat nostrum cupiditate qui minus!
+</p>
+
+<p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+    Ex esse sed quis ad quo, qui atque voluptates quidem a minima dignissimos iusto placeat illo,
+    totam, ut aspernatur odit reprehenderit! Repudiandae?
+</p>
